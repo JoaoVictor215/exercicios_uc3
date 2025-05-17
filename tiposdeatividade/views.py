598081b2aa3d868b011cfc3b8f0d7ad4558bc16e
João Victor Cozza V. Fonseca <joaovictorcozza19@gmail.com>
2025-05-16 21:24:13 -0300

    atualizacao

--- tiposdeatividade/views.py
+++ tiposdeatividade/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Olá! Eu sou o index")
+
+def exibe_mensagem(request):
+    t_html = ""
+    return HttpResponse(t_html)
+
+'<!DOCType html><body>Olá</body></!DOCType>'
