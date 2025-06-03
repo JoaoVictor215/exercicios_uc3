9c39d15bedf80bfbd2f9b3a1471f79de679d099d
João Victor Cozza V. Fonseca <joaovictorcozza19@gmail.com>
2025-06-02 21:37:12 -0300

    Inicio de outra coisa

--- titulo/models.py
+++ titulo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Titulo(models.Model):
+    codigo = models.AutoField(primary_key=True, help_text='Digite o Código')
+    descricao = models.CharField(max_length=100, help_text='Informe a descrição do Titulo')
+
+def __str__(self):
+    return f'{self.codigo} - {self.descricao}'
--- turma/models.py
+++ turma/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    numero = models.AutoField(primary_key=True, help_text='Digite o Numero')
+
+
+
+
+
+def __str__(self):
+    return f'{self.} - {self.}'
